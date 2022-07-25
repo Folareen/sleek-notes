4677770d186bc3101725995e55d96d0315b0c5cf
Folareen <sakawahab03@gmail.com>
2022-07-25 23:54:44 +0100

    dependencies

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edit: true,
+    };
+  }
+
+  changeValue = () => {
+    this.setState({ edit: !this.state.edit });
+  };
+
+  componentDidUpdate(){
+    if(this.state.edit){
+      document.querySelector('textarea').disabled = false;
+      document.querySelector(".state").textContent = "state is true"  
+    }else{
+      document.querySelector('textarea').disabled = true;
+      document.querySelector(".state").textContent = "state is false";
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p className="state"></p>
+        <textarea >heyyyy</textarea>
+        <button onClick={this.changeValue}>change number</button>
+      </div>
+    );
+  }
+}
