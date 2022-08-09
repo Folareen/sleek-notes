9d20673ebca4b1bd74edb2193e88d0c4b9b6c5f9
Folareen <sakawahab03@gmail.com>
2022-08-09 16:09:31 +0100

    theme palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
 
 export default class App extends Component {
   constructor(props) {
@@ -10,12 +12,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00204A",
+    },
+    secondary: {
+      main: "#005792",
+    },
+    info: {
+      main: "#BAE8E8",
+    },
+    warning: {
+      main: "#E33E5A",
+    },
+    success: {
+      main: "#21ABA5",
+    },
+  },
+});
+
+export default theme;
