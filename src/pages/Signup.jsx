69245738f8919ad87e76b3f22fdf3da5eb149913
Folareen <sakawahab03@gmail.com>
2022-08-10 13:32:44 +0100

    adjustments

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,7 +8,7 @@ export default class Login extends Component {
     <Grid container direction="column" justifyContent="center" alignItems="center" height="100vh"
     >
       
-      <Paper elevation={4} sx={{ width: '75%', maxWidth: 400, p:3}} >
+      <Paper elevation={4} sx={{ width: '80%', maxWidth: 400, p:3}} >
 
       <Typography variant="h2" gutterBottom align={'center'} color='primary.main' sx={{fontSize: 40, fontWeight: 'bold', mb:2}}>
         Notes App
@@ -29,7 +29,7 @@ export default class Login extends Component {
 
           <Button variant="contained"  align='center' type='submit' sx={{my: 2, py: 2, fontWeight: 'bold', bgcolor:'secondary.main'}} >Sign Up</Button>
 
-          <Container sx={{display: 'flex', justifyContent:'center', alignItems:'center'}} fullwidth align='center'>
+          <Container sx={{display: 'flex', justifyContent:'center', alignItems:'center'}} fullwidth >
             <Typography component='p' sx={{p:0.5}}>
               Have an account?
             </Typography>
