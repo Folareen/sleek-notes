9a87c024ca5a27689eac0db428fe416c57b07409
Folareen <sakawahab03@gmail.com>
2022-08-10 13:14:11 +0100

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,39 @@
 import React, { Component } from 'react'
-import { Grid,Typography, Paper, TextField, Container, Button, FormControl } from '@mui/material'
-// import { Link } from 'react-router-dom'
+import { Grid,Typography, Paper, TextField, Container, Button, FormControl, Link } from '@mui/material'
+import {Link as RouterLink} from 'react-router-dom'
 
 export default class Login extends Component {
   render() {
     return (
-    <Grid container direction="column" justifyContent="center" alignItems="center" height="100vh" sx={{border: 3, }}
+    <Grid container direction="column" justifyContent="center" alignItems="center" height="100vh"
     >
-
-      <Typography variant="h2" gutterBottom align={'center'} color='primary.main'>
+      <Typography variant="h2" gutterBottom align={'center'} color='primary.main' sx={{fontSize: 40, fontWeight: 'bold'}}>
         Notes App
       </Typography>
+      <Paper elevation={4} sx={{ width: '75%', maxWidth: 400, p:4}} >
 
-      <Paper elevation={4} sx={{ width: '75%', maxWidth: 450, p:4}} >
-
-        <Typography variant="h3" gutterBottom align='center'>
+        <Typography variant="h3" align='center' sx={{fontSize: 30, fontWeight: 'bold', mb:2}} >
           Login
         </Typography>
 
         <FormControl component='form' fullWidth>
-          <TextField required id="outlined-required" label="Email Address" sx={{my: 2}} fullWidth 
+          <TextField required id="outlined-required" label="Email Address" sx={{my: 2, color:'info.main' }} fullWidth 
           />
           <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" fullWidth sx={{my: 2}}
           />        
 
-          <Button variant="contained" align='center' type='submit' sx={{my: 2, py: 2}}>Login</Button>
+          <Button variant="contained"  align='center' type='submit' sx={{my: 2, py: 2, fontWeight: 'bold', bgcolor:'secondary.main'}} >Login</Button>
 
-          <Container sx={{display: 'flex', border: 1}} fullwidth align='center'>
-            <Typography>
+          <Container sx={{display: 'flex', justifyContent:'center', alignItems:'center'}} fullwidth align='center'>
+            <Typography component='p' sx={{p:0.5}}>
               Don't have an account?
             </Typography>
-            <Button href='/signup' >
+            <Link to='/signup' component={RouterLink}  sx={{color:'primary.main', p:0.5, bgcolor:'info.main'}} underline="none" >
               Signup
-            </Button>
+            </Link>
           </Container>
-
         
         </FormControl>
-
 
       </Paper>
     </Grid>
