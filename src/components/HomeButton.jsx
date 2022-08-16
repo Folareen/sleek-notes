2481d58136a342d94e01b2e180032f76a775e9b7
Folareen <sakawahab03@gmail.com>
2022-08-16 17:51:24 +0100

    changed homebutton styles

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -5,9 +5,9 @@ import HomeIcon from '@mui/icons-material/Home';
 
 const HomeButton = () => {
     return (
-            <Link to='/' component={RouterLink}  sx={{ color:'light.main', px: 4}} underline="none" >
-              <HomeIcon />
-            </Link>
+      <Link to='/' component={RouterLink}  sx={{ color:'background.paper', px:4}} underline="none" >
+        <HomeIcon sx={{fontSize: 36}} />
+      </Link>
     )
 }
 
