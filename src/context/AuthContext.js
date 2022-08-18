a02de3326c578d875e78ea71089e315916dc33f6
Folareen <sakawahab03@gmail.com>
2022-08-18 22:31:40 +0100

    fixed authcontext bug

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,7 +5,11 @@ export const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState();
 
-  return <AuthContext value={{ user, setUser }}>{children}</AuthContext>;
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
