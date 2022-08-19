6e97729c9133021237ba4720acc7be5692217e5f
Folareen <sakawahab03@gmail.com>
2022-08-19 20:25:37 +0100

    logout

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import { IconButton } from '@mui/material';
+import { signOut } from 'firebase/auth';
+import { auth} from '../firebase'
+import { AuthContext } from '../context/AuthContext';
 
 export default function LogoutButton (){
+    const { setUser} = useContext(AuthContext) 
+
+    const logout = async () => {
+
+        try{
+            await signOut(auth)
+            setUser(null) 
+            console.log('out')   
+        }
+        catch{
+            alert('failed to sign out')
+        }
+    }
+
+
     return (
-        <IconButton sx={{ mr: 0, ml: 2, color:"error.dark", border: 1, bgolor: 'error.dark'}} >
+        <IconButton sx={{ mr: 0, ml: 2, color:"error.dark", border: 1, bgolor: 'error.dark'}} onClick={logout} >
             <LogoutRoundedIcon />
         </IconButton>
     )
