9a4f819ab27874c424daeed1cf2e955419cfd136
Folareen <sakawahab03@gmail.com>
2022-08-21 13:01:07 +0100

    added documents contextprovider

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import DocumentsPreview from "../pages/DocumentsPreview";
 import DocumentFullView from "../pages/DocumentFullView";
 import {Routes, Route } from 'react-router-dom';
+import DocumentsContextProvider from '../context/DocumentsContext';
 
 
 const PrivateRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<DocumentsPreview />} />
-      <Route path="/:id" element={<DocumentFullView />} />
-    </Routes>
+    <DocumentsContextProvider>
+      <Routes>
+        <Route path="/" element={<DocumentsPreview />} />
+        <Route path="/:id" element={<DocumentFullView />} />
+      </Routes>
+    </DocumentsContextProvider>
+
   )
 }
 
